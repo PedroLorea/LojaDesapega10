482f42e94cc48fa24564d71048d6de4aceff3df6
PedroLorea <p.lorea@edu.pucrs.br>
2022-11-23 20:38:50 -0300

    Adicionar arquivos de projeto.

---
+++ LojaDesapega10/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaDesapega10.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
+
